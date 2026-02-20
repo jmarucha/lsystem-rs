fabc700bd7ce5d0b0966af51c79c141125129db0
Jan K. Marucha <jkmarucha@gmail.com>
2026-02-20 17:16:33 +0100

    refactor rendering logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod lsystem;
 mod lsystem_test;
+mod render;
 
 use std::f32::consts::PI;
 
@@ -16,6 +17,8 @@ use nalgebra::Perspective3;
 use nalgebra as na;
 use nalgebra::Point3;
 
+use crate::render::Render;
+
 const RDEPTH: i32 = 10;
 
 fn main() {
@@ -28,64 +31,10 @@ fn main() {
         .with_title("Glium tutorial #2")
         .build(&event_loop);
 
-    #[derive(Copy, Clone, Debug)]
-    struct Vertex {
-        position: [f32;2]
-    }
-    implement_vertex!(Vertex, position);
-
-    //let perspective = na::Perspective3::new(4./3., PI/3., -1., 1.).into_inner();
-    //let perspective_raw: [[f32;4]; 4] = perspective.into();
-    let perspective_raw = [
-        [0.1, 0.0, 0.0, 0.0],
-        [0.0, 0.1, 0.0, 0.0],
-        [0.0, 0.0, 0.1, 0.0],
-        [0.0, 0.0, 2.0, 1.0f32]
-    ];
-
-    let shape_iter = get_points_bfs(&test_actually_nice_tree(), RDEPTH).into_iter().map(|point: Point3<_>| -> Vertex {Vertex { position: [point[0],point[1]] }});
-    let shape = Vec::from_iter(shape_iter);
-;
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
-    //let indices = glium::index::NoIndices(glium::index::PrimitiveType::Points);
-
-    let vertex_shader_src = r#"
-        #version 140
-
-        in vec2 position;
-        // uniform mat4 perspective;
-        uniform float scale;
-
-        #define SCALE 2.0
-        #define AR 4.0/3.0
-
-        void main() {
-            // gl_Position = vec4(position.x/SCALE, position.y*AR/SCALE-0.6, 0.0, 1.0);
-            gl_Position = vec4(position.x/scale, position.y*AR/scale-0.6, 0.0, 1.0);
-
-            //gl_Position = perspective*vec4(position.x, position.y, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-
-        out vec4 color;
-        // uniform vec3 csetting;
-
-        void main() {
-            color = vec4(1.0, 0., 0., 1.0);
-        }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    let mut target = display.draw();
-    target.clear_color(0.0, 0.0, 0.0, 1.0);
-    target.draw(&vertex_buffer, &indices, &program, &uniform!{scale: 2.0f32},
-        &Default::default()).unwrap();
-    target.finish().unwrap();
+    let points = get_points_bfs(&test_actually_nice_tree(), RDEPTH);
+    let render = Render::init_render(display);
+    render.draw(points);
+    
 
     #[allow(deprecated)]
     event_loop.run(move |ev, window_target| {
--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,81 @@
 
+use glium::{Program, Surface, backend::glutin::Display, glutin::surface::WindowSurface, index::NoIndices};
+use nalgebra::Point3;
 
-#[derive(Copy, Clone)]
+
+#[derive(Copy, Clone, Debug)]
 struct Vertex {
-    position: [f32; 2],
+    position: [f32;2]
 }
 implement_vertex!(Vertex, position);
+
+pub struct Render {
+    display: Display<WindowSurface>,
+    indices: NoIndices,
+    program: Program,
+}
+
+impl Render {
+    pub fn init_render(display: Display<WindowSurface>) -> Self {
+        //let perspective = na::Perspective3::new(4./3., PI/3., -1., 1.).into_inner();
+        //let perspective_raw: [[f32;4]; 4] = perspective.into();
+        let perspective_raw = [
+            [0.1, 0.0, 0.0, 0.0],
+            [0.0, 0.1, 0.0, 0.0],
+            [0.0, 0.0, 0.1, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32]
+        ];
+
+
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
+        //let indices = glium::index::NoIndices(glium::index::PrimitiveType::Points);
+
+        let vertex_shader_src = r#"
+            #version 140
+
+            in vec2 position;
+            // uniform mat4 perspective;
+            uniform float scale;
+
+            #define SCALE 2.0
+            #define AR 4.0/3.0
+
+            void main() {
+                // gl_Position = vec4(position.x/SCALE, position.y*AR/SCALE-0.6, 0.0, 1.0);
+                gl_Position = vec4(position.x/scale, position.y*AR/scale-0.6, 0.0, 1.0);
+
+                //gl_Position = perspective*vec4(position.x, position.y, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 140
+
+            out vec4 color;
+            // uniform vec3 csetting;
+
+            void main() {
+                color = vec4(1.0, 0., 0., 1.0);
+            }
+        "#;
+
+        let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+        Render {display, indices, program}
+    }
+
+    pub fn draw(self: &Self, points: Vec<Point3<f32>>) -> () {
+        // convert to vertex buffer
+        let shape_iter = points.into_iter().map(|point: Point3<_>| -> Vertex {Vertex { position: [point[0],point[1]] }});
+        let shape = Vec::from_iter(shape_iter);
+        let vertex_buffer = glium::VertexBuffer::new(&self.display, &shape).unwrap();
+
+        // draw
+        let mut target = self.display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.draw(&vertex_buffer, &self.indices, &self.program, &uniform!{scale: 2.0f32},
+            &Default::default()).unwrap();
+        target.finish().unwrap();
+
+    }
+
+}
